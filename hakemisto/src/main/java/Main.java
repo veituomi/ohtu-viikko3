c9a22ec2b6962efd586e9a4a6159001b19b4d69e
Veini Tuominen <veini.tuominen@hotmail.com>
2017-03-31 22:53:43 +0300

    2. lisää gradlea: koodin staattinen analyysi

--- hakemisto/src/main/java/Main.java
+++ hakemisto/src/main/java/Main.java
@@ -2,9 +2,11 @@ import java.util.*;
 import ohtu.Multiplier;
 
 public class Main {
+    private static final int three = 3; 
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Multiplier kolme = new Multiplier(3);
+        Multiplier kolme = new Multiplier(three);
         System.out.println("anna luku ");
         int luku = scanner.nextInt();
 
